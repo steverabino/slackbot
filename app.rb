245cf8594ed26c919bb1a4b438883923a453f204
Steve Smith <stevesmith2609@gmail.com>
2017-03-10 17:24:06 +0000

    Fix bugs

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require 'sinatra'
 require 'httparty'
 require 'json'
 
-post '/gateway'  do
-  message = params[:text].gsub(parame[:trigger_word], '').strip
+post '/gateway' do
+  message = params[:text].gsub(params[:trigger_word], '').strip
 
   action, repo = message.split('_').map { |c| c.strip.downcase }
   repo_url = "https://api.github.com/repos/#{repo}"
@@ -12,11 +12,11 @@ post '/gateway'  do
   when 'issues'
     resp = HTTParty.get(repo_url)
     resp = JSON.parse resp.body
-    respond message "There are #{resp['open_issues_count']} open issues on #{repo}"
+    respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
   end
 end
 
-def respond_message
+def respond_message message
   content_type :json
   {:text => message}.to_json
 end
